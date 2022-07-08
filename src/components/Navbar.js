bd95b3b8396631118e73713eafc4f803ecc35691
rahul <rs2570089@gmail.com>
2022-07-08 21:51:36 +0530

    Updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import '../App.css'
+import photo from '../images/logo.jpeg'
+
 
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark">
             <div className="container-fluid">
                 <NavLink className="navbar-brand mx-3" to="/">
-                    Rahul Shinde
+                    <img src={photo} alt="" height={50} width={50} style={{borderRadius: '100px'}}/>
                 </NavLink>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
@@ -15,19 +17,19 @@ const Navbar = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0 mx-auto">
                         <li className="nav-item mx-3">
-                            <NavLink className="nav-link" data-bs-toggle="collapse" aria-current="page" to="/">About me</NavLink>
+                            <NavLink className="nav-link" aria-current="page" to="/">About me</NavLink>
                         </li>
                         <li className="nav-item mx-3">
-                            <NavLink className="nav-link" data-bs-toggle="collapse" to="/education">Education</NavLink>
+                            <NavLink className="nav-link" to="/education">Education</NavLink>
                         </li>
                         <li className="nav-item mx-3">
-                            <NavLink to="/projects" data-bs-toggle="collapse" className='nav-link'>Projects</NavLink>
+                            <NavLink to="/projects" className='nav-link'>Projects</NavLink>
                         </li>
                         <li className="nav- mx-3">
-                            <NavLink to="/achievements" data-bs-toggle="collapse" className='nav-link'>Achievements</NavLink>
+                            <NavLink to="/achievements" className='nav-link'>Achievements</NavLink>
                         </li>
                         <li className="nav-item mx-3">
-                            <NavLink to="/contactme" data-bs-toggle="collapse" className='nav-link'>Contact me</NavLink>
+                            <NavLink to="/contactme" className='nav-link'>Contact me</NavLink>
                         </li>
                     </ul>
                 </div>
