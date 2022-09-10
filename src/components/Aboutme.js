157258efc17a81ca195e7cfabc6525a1333606f8
rahul <rs2570089@gmail.com>
2022-09-10 11:55:58 +0530

    Updated

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -8,13 +8,13 @@ const Aboutme = () => {
       <div className="back">
         <div className='divv'>
           <img className='circle' src={photo} width={200} height={200} alt="" />
-          <h1 className='hone'>I'm Rahul Shinde<br></br><p className='h5'>I'm into Flutter development, web development and cloud computing</p></h1>
+          <h1 className='hone'>I'm Rahul Shinde<br></br><p className='h5'>I'm into Mobile App (Flutter) development majorly and cloud</p></h1>
         </div>
         <div className="p1">
           <h1>About me</h1>
           <p className='p2'>
             I am Rahul Shinde currently pursuing Bachelor of Technology in Information Technology from Dharmsinh Desai University, Nadiad, Gujarat.
-            I am into Flutter development (mainly for android), web development (front-end) and cloud (GCP).
+            I am into Mobile App (Flutter) development majorly and cloud (GCP and AWS).
           </p>
         </div>
       </div>
